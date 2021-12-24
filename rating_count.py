86e96a272d1c49392c6a6aa543021b5862d47f65
Nishkarsh Vardhan <nvardhan@twilio.com>
2021-12-24 19:00:50 +0530

    example 2

--- rating_count.py
+++ rating_count.py
@@ -5,6 +5,7 @@ conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
 sc = SparkContext(conf = conf)
 
 lines = sc.textFile("ml-100k/u.data")
+
 ratings = lines.map(lambda x: x.split()[2])
 result = ratings.countByValue()
 
